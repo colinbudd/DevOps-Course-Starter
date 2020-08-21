f7d48b1d50b218387190829c0a6405d9fca2842a
Colin Budd <colin.budd@capita.com>
2020-08-21 10:48:36 +0100

    Display the list of saved todo items Create new todo items

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', tasks = session.get_items())
+
+@app.route('/', methods=['POST'])
+def add_todo():
+    session.add_item(request.form.get('title'))
+    return index()
 
 if __name__ == '__main__':
     app.run()
