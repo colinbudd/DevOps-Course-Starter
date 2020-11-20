f34666f4cbbaf15aa6b854d782d49ed4ba40812f
Colin Budd <colin.budd@capita.com>
2020-11-20 17:00:52 +0000

    Wait for page reload

--- test_selenium.py
+++ test_selenium.py
@@ -2,6 +2,9 @@ import os
 import pytest
 from threading import Thread
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 from app import create_app
 from board import create_trello_board, delete_trello_board
 
@@ -36,10 +39,20 @@ def test_task_journey(driver, test_app):
 
     assert driver.title == 'To-Do App'
 
+    todo_title = 'Test Todo Item'
+    todo_description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
     # Create new item
 
-    title = driver.find_element_by_id('title')
-    title.send_keys('Test Todo Item')
+    driver.find_element_by_id('title').send_keys(todo_title)
+    driver.find_element_by_id('description').send_keys(todo_description)
+    driver.find_element_by_id('add-new-todo').click()
 
-    description = driver.find_element_by_id('description')
-    description.send_keys('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    # Wait for the page reload
+
+    w = WebDriverWait(driver, 8)
+    w.until(expected_conditions.presence_of_element_located((By.XPATH, f'//*[contains(@class, "todo-title")][text() = "{todo_title}"]')))
+
+    # Check new item created
+
+    
