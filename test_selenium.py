1514a9af865459ac8cf5b3849c028e1ff8439426
Colin Budd <colin.budd@capita.com>
2020-11-20 16:00:15 +0000

    Selenium is working

--- test_selenium.py
+++ test_selenium.py
@@ -30,3 +30,16 @@ def test_app():
 def driver():
     with webdriver.Firefox() as driver:
         yield driver
+
+def test_task_journey(driver, test_app):
+    driver.get('http://localhost:5000/')
+
+    assert driver.title == 'To-Do App'
+
+    # Create new item
+
+    title = driver.find_element_by_id('title')
+    title.send_keys('Test Todo Item')
+
+    description = driver.find_element_by_id('description')
+    description.send_keys('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
